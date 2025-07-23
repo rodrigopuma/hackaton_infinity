6831daaa5ae40308dd2bc5e6b63ad8cfe9ef1093
lrpuma <rodrigopumalima@gmail.com>
2025-07-23 14:55:32 -0300

    Atualização da rota da página de "Registro"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 // src/App.jsx
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import DashboardPage from './pages/DashboardPage';
-import LoginPage from './pages/LoginPage';
-import ProfilePage from './pages/ProfilePage';
-import { ThemeProvider } from './contexts/ThemeContext';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DashboardPage from "./pages/DashboardPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Register from "./pages/registerPage";
 
 function App() {
   return (
@@ -18,13 +19,16 @@ function App() {
             {/* Rota para a página de Login */}
             <Route path="/login" element={<LoginPage />} />
 
+            {/* Rota para a página de Registro */}
+            <Route path="/registrar" element={<Register />} />
+
             {/* Rota para a página de Perfil */}
             <Route path="/perfil" element={<ProfilePage />} />
           </Routes>
         </BrowserRouter>
       </div>
     </ThemeProvider>
-  )
+  );
 }
 
 export default App;
