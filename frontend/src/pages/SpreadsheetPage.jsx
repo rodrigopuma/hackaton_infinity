a0cd9520dd2ce8605170e577f3d7debfd9f934c7
rodrigopuma <rodrigopuma.dev@outlook.com>
2025-08-03 23:18:44 -0300

    fix: Atualizando tela das planilhas

--- frontend/src/pages/SpreadsheetPage.jsx
+++ frontend/src/pages/SpreadsheetPage.jsx
@@ -1,24 +1,28 @@
 function SpreadsheetPage() {
-    // COLE AQUI O LINK QUE VOCÊ COPIOU DO GOOGLE SHEETS
-    const spreadsheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRGZ8w8gFLoPbdGJK_nCvtW9REJD8hTvGlYMiU6Hgb7CYVwanwcJgclwS5eCvYheTUy7lvjKTOAvUtb/pubhtml?widget=true&amp;headers=false";
+  // COLE AQUI O LINK QUE VOCÊ COPIOU DO GOOGLE SHEETS
+  const spreadsheetUrl =
+    "https://docs.google.com/spreadsheets/d/1FvmL5WmEeEoNtT9qaWAkwAg2o7MZ1XMuwM9kSUZJixk/edit?usp=sharing";
 
-    return (
-        <div>
-            <h1 className="text-3xl font-bold text-gray-800 dark:text-infinity-text mb-6">
-                Planejamento Pedagógico
-            </h1>
-            <div className="w-full h-[75vh] bg-white rounded-lg shadow-lg">
-                {/* O iframe é o que "embuta" a página da planilha aqui dentro */}
-                <iframe
-                    src={spreadsheetUrl}
-                    title="Planilha de Planejamento"
-                    className="w-full h-full border-0 rounded-lg"
-                >
-                    Carregando planilha...
-                </iframe>
-            </div>
-        </div>
-    );
+  const idClienteAPI =
+    "605775581583-8fpokkapkgb306g4j7n4v47e3k5in5q6.apps.googleusercontent.com";
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-gray-800 dark:text-infinity-text mb-6">
+        Planejamento Pedagógico
+      </h1>
+      <div className="w-full h-[75vh] bg-white rounded-lg shadow-lg">
+        {/* O iframe é o que "embuta" a página da planilha aqui dentro */}
+        <iframe
+          src={spreadsheetUrl}
+          title="Planilha de Planejamento"
+          className="w-full h-full border-0 rounded-lg"
+        >
+          Carregando planilha...
+        </iframe>
+      </div>
+    </div>
+  );
 }
 
 export default SpreadsheetPage;
